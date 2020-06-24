166a634eea8f157d90d765a0244219e03b052e95
JealousGx <59867217+JealousGx@users.noreply.github.com>
2020-06-24 12:48:19 +0500

    2 additional buttons plus temperature increment/decrement

--- src/Room.js
+++ src/Room.js
@@ -3,6 +3,8 @@ import './Room.css';
 
 function Room() {
     const state = useState(true);
+    let [temp, increaseTemp] = useState(22);
+
     let isLit = state[0];
     let setLit = state[1];
     function updateLit() {
@@ -11,7 +13,12 @@ function Room() {
   return (
     <div className = {`room ${isLit ? "lit" : "dark"}`}>
       Jealous' room is {isLit ? 'lit' : 'dark'}.<br />
-      <button onClick = {() => updateLit()}>Switch</button>
+      <button onClick = {() => updateLit()}>Switch</button><br/>
+      <button onClick = {() => setLit(isLit = true)}>Turn on</button>
+      <button onClick = {() => setLit(isLit = false)}>Turn off</button><br/><br/><br/>
+      Current temperature: {temp}.<br/>
+      <button onClick = {() => increaseTemp(++temp)}>Increase temperature</button><br/>
+      <button onClick = {() => increaseTemp(--temp)}>Decrease temperature</button>
     </div>
   );
 }
