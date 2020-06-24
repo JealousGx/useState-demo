7a4d846da3b1c94fdffd6cfff1e1fd15083e94dd
JealousGx <59867217+JealousGx@users.noreply.github.com>
2020-06-24 12:22:17 +0500

    Lighting my room

--- src/Room.js
+++ src/Room.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
+import './Room.css';
 
 function Room() {
     const state = useState(true);
     let isLit = state[0];
     let setLit = state[1];
     function updateLit() {
-       setLit(!isLit);yyyyyy
+       setLit(!isLit);
     }
   return (
-    <div>
+    <div className = {`room ${isLit ? "lit" : "dark"}`}>
       Jealous' room is {isLit ? 'lit' : 'dark'}.<br />
       <button onClick = {() => updateLit()}>Switch</button>
     </div>
